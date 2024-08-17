022c04c2801bde2512320c0e013c5bb45414c90d
Loren Wu <loren13wu@gmail.com>
2024-08-17 11:16:48 -0700

    minor edits to title

--- client/src/components/Title.tsx
+++ client/src/components/Title.tsx
@@ -3,9 +3,7 @@ import '../assets/styles/title.css';
 const Title = () => {
   return (
     <div className='title-container'>
-      <h3>Welcome to</h3>
-      {/* <h4>to</h4> */}
-      <h1>Fridge To Feast</h1>
+      <h1>Fridge to Feast</h1>
     </div>
   );
 };
