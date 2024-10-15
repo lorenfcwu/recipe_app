15e5a887548bc0e80ed79aedeed98a44532d9a21
Loren Wu <loren13wu@gmail.com>
2024-10-15 15:41:01 -0700

    Changed BrowserRouter to HashRouter for GitHub Pages support

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { Home, Search } from './pages';
 import './assets/styles/global.css';
 
